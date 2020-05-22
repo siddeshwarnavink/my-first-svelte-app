ee8cfbc33d97fcba8f33de8744a92af12c532b1a
Siddeshwar Navinkumar <admin@siddeshrocks.in>
2020-05-22 08:44:19 +0530

    added the dynamic component

---
+++ src/CarItem.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+  export let speed;
+</script>
+
+<div class="car_item">
+  <span class="car_item--title">{title}</span>
+  <span class="car_item--speed">Speed: {speed}</span>
+</div>
