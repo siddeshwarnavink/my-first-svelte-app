35c5b8cbcf23b8e25fdc28fc2a1c2e327b6b84bb
Siddeshwar Navinkumar <admin@siddeshrocks.in>
2020-05-22 08:36:09 +0530

    added a static header component

---
+++ src/TheHeader.svelte
@@ -0,0 +1,16 @@
+<style>
+  .header {
+    padding: 10px;
+    background-color: #304ffe;
+    color: #fff;
+  }
+
+  .header--title {
+    font-size: 25px;
+    margin-left: 5px;
+  }
+</style>
+
+<header class="header">
+  <span class="header--title">My First Svelte app!</span>
+</header>
