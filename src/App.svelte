0f61c69b8add1c984c22fd0f7ca0008c420c627d
Siddeshwar Navinkumar <admin@siddeshrocks.in>
2020-05-22 17:03:07 +0530

    added basic form

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import data from './initData.js';
   import Header from "./TheHeader.svelte";
   import CarItem from "./CarItem.svelte";
+  import NewCar from './NewCar.svelte'
 </script>
 
 <style>
@@ -16,6 +17,7 @@
   <Header />
 
   <div class="content">
+    <NewCar />
     {#each data as carData}
       <CarItem {...carData} />
     {/each}
---
+++ src/NewCar.svelte
@@ -0,0 +1,56 @@
+<style>
+  .new_car {
+    padding: 10px;
+    border-radius: 10px;
+    border: 1px solid #333;
+    margin-bottom: 12px;
+  }
+
+  .new_car .new_car--title {
+    font-size: 24px;
+    display: block;
+    padding: 10px 0px;
+  }
+
+  .new_car .new_car--formfield label {
+    font-weight: bold;
+  }
+
+  .new_car .new_car--formfield input {
+    display: block;
+    font-size: 16px;
+    padding: 2px 1px;
+    margin-bottom: 10px;
+  }
+
+  .new_car button {
+    border: 0;
+    background-color: transparent;
+    text-transform: uppercase;
+    outline: 0;
+    cursor: pointer;
+    color: #304ffe;
+    font-weight: bold;
+  }
+</style>
+
+<div class="new_car">
+  <span class="new_car--title">Add new car</span>
+  <form action="">
+    <div class="new_car--formfield">
+      <label for="titleField">Title:</label>
+      <input
+        type="text"
+        name="title"
+        id="titleField"
+        placeholder="e.g: Race Car" />
+    </div>
+
+    <div class="new_car--formfield">
+      <label for="titleField">Speed:</label>
+      <input type="text" name="title" id="titleField" placeholder="e.g: 550" />
+    </div>
+
+    <button>Add car</button>
+  </form>
+</div>
