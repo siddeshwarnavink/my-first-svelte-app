650cdd9d87232ea5f002bbf2d07e1e4570f108d1
Siddeshwar Navinkumar <admin@siddeshrocks.in>
2020-05-23 08:27:42 +0530

    doing all the async CRUD function

--- src/store/cars.js
+++ src/store/cars.js
@@ -2,6 +2,26 @@ import { writable } from 'svelte/store';
 
 const carList = writable([]);
 
+// Helper Function
+const optimisticallyUpdate = (updateCallback) => {
+    const oldCarList = carList;
+
+    updateCallback();
+
+    // Performing optimistic update
+    fetch('https://api.jsonbin.io/b/5ec88819e91d1e45d10fa2d5', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'secret-key': '$2b$10$M2d8feAXLHKRTQsA6oy43O.YwacaoHtAGx72UBT69Cd4ETI4F0rWy'
+        },
+        body: JSON.stringify({ "cars": carList })
+    }).catch((err) => {
+        console.log(err);
+        carList.set(oldCarList);
+    });
+}
+
 const fetchCars = async () => {
     const res = await fetch('https://api.jsonbin.io/b/5ec88819e91d1e45d10fa2d5', {
         headers: {
@@ -13,15 +33,15 @@ const fetchCars = async () => {
     carList.set(loadedCars);
 }
 
-const addCar = car => carList.update(cars => [car, ...cars]);
+const addCar = car => optimisticallyUpdate(() => carList.update(cars => [car, ...cars]));
 
-const editCar = newCar => carList.update(cars => cars.map(c => {
+const editCar = newCar => optimisticallyUpdate(() => carList.update(cars => cars.map(c => {
     if (c.id == newCar.id) {
         return newCar;
     }
     return c;
-}));
+})));
 
-const deleteCar = carId => carList.update(cars => cars.filter(({ id }) => id != carId));
+const deleteCar = carId => optimisticallyUpdate(() => carList.update(cars => cars.filter(({ id }) => id != carId)));
 
 export { deleteCar, editCar, addCar, carList, fetchCars };
