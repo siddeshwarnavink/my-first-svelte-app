58cf6d68028cc4ca08c1b2c4cad7f05c6a91974e
Siddeshwar Navinkumar <admin@siddeshrocks.in>
2020-05-23 08:04:05 +0530

    displaying loading message

--- src/store/cars.js
+++ src/store/cars.js
@@ -1,7 +1,17 @@
 import { writable } from 'svelte/store';
-import carData from './initData';
 
-const carList = writable(carData);
+const carList = writable([]);
+
+const fetchCars = async () => {
+    const res = await fetch('https://api.jsonbin.io/b/5ec88819e91d1e45d10fa2d5', {
+        headers: {
+            'secret-key': '$2b$10$M2d8feAXLHKRTQsA6oy43O.YwacaoHtAGx72UBT69Cd4ETI4F0rWy'
+        }
+    });
+    const data = await res.json();
+    const loadedCars = data.cars
+    carList.set(loadedCars);
+}
 
 const addCar = car => carList.update(cars => [car, ...cars]);
 
@@ -14,4 +24,4 @@ const editCar = newCar => carList.update(cars => cars.map(c => {
 
 const deleteCar = carId => carList.update(cars => cars.filter(({ id }) => id != carId));
 
-export { deleteCar, editCar, addCar, carList };
+export { deleteCar, editCar, addCar, carList, fetchCars };
